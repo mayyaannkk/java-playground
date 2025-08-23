21264771e1c662073174b04eae8e00002d25789a
mayyaannkk <mayankpant.dev@gmail.com>
2025-08-23 07:49:45 +0530

    chore: initial project setup and util classes

---
+++ src/main/java/com/mayank/java/util/entities/Project.java
@@ -0,0 +1,18 @@
+package com.mayank.java.util.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Project{
+
+    private String projectCode;
+    private String name ;
+    private String client;
+    private String buLeadName;
+}
